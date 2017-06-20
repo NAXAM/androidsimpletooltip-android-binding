5533597bfbdab7131e9064d71ce853dcd44b284d
Do Thanh Binh <binh@naxam.net>
2017-06-20 11:25:57 +0700

    done register

---
+++ Naxam.Busuu.Droid/Setup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MvvmCross.Droid.Platform;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform.Platform;
+using System.Reflection;
+using Naxam.Busuu.Droid.Profile.Views;
+using Naxam.Busuu.Profile.ViewModel;
+using Naxam.Busuu.Core.Converter;
+using System.Collections;
+
+namespace Naxam.Busuu.Droid
+{
+    public class Setup : MvxAndroidSetup
+    {
+        public Setup(Context applicationContext) : base(applicationContext)
+        {
+        }
+       
+        protected override void InitializeLastChance()
+        {
+            base.InitializeLastChance();
+            MvvmCross.Plugins.File.PluginLoader.Instance.EnsureLoaded();
+            MvvmCross.Plugins.DownloadCache.PluginLoader.Instance.EnsureLoaded();
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new App();
+        }
+
+        protected override IMvxTrace CreateDebugTrace()
+        {
+            return new DebugTrace();
+        }
+
+        protected override IEnumerable<Assembly> ValueConverterAssemblies
+        {
+            get
+            {
+                var toReturn = base.ValueConverterAssemblies as IList;
+                toReturn.Add(typeof(IsMatchPatternBase64Converter).Assembly); 
+                return (IEnumerable<Assembly>)toReturn; 
+                 
+            }
+        }
+        protected override IEnumerable<Assembly> GetViewModelAssemblies()
+        {
+            var list = new List<Assembly>();
+            list.AddRange(base.GetViewModelAssemblies());
+            list.Add(typeof(ForgotPasswordViewModel).Assembly);
+            return list.ToArray();
+        }
+
+        protected override IEnumerable<Assembly> GetViewAssemblies()
+        {
+            var list = new List<Assembly>();
+            list.AddRange(base.GetViewAssemblies());
+            list.Add(typeof(ForgotPasswordView).Assembly);
+            return list.ToArray();
+        }
+
+        protected override IEnumerable<Assembly> AndroidViewAssemblies
+    => new List<Assembly>(base.AndroidViewAssemblies)
+{
+    typeof(MvvmCross.Binding.Droid.Views.MvxImageView).Assembly
+};
+    }
+}
---
+++ Naxam.Busuu.Profile/Service/IDialogService.cs
@@ -0,0 +1,14 @@
+﻿using Naxam.Busuu.Profile.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naxam.Busuu.Profile.Service
+{
+    public interface IDialogService
+    {
+        Task<bool> ConfirmChooseLanguage(LanguageModel lang);
+    }
+}
