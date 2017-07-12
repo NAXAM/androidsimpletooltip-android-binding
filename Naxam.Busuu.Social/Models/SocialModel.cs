9fb82e15d1e8ca49a9c7e238b6ce1cbb27e9e451
Nguyen Nhu Son <son.nguyennhu@naxam.net>
2017-07-12 13:23:15 +0700

    fix bug lan 2.

--- Naxam.Busuu.Social/Models/SocialModel.cs
+++ Naxam.Busuu.Social/Models/SocialModel.cs
@@ -1,20 +1,217 @@
-﻿namespace Naxam.Busuu.Social.Models
+﻿using MvvmCross.Core.ViewModels;
+
+namespace Naxam.Busuu.Social.Models
 {
-    public class SocialModel
+    public class SocialModel : MvxNotifyPropertyChanged
     {
-		public int Id { get; set; }
-		public string Avatar { get; set; }
-		public string Name { get; set; }
-		public string Country { get; set; }
-		public string ImageSpeakLanguage { get; set; }
-		public string ImageLearn { get; set; }
-		public string TextLearn { get; set; }
-		public bool Speak { get; set; }
-		public string Write { get; set; }
-		public string PublicTime { get; set; }
-        public double Star { get; set; }
-        public bool Friends { get; set; }
-        public string ImgQuestion { get; set; }
-        public string TextQuestion { get; set; }
+        int _Id;
+
+        public int Id
+        {
+            get { return _Id; }
+            set
+            {
+                if (_Id != value)
+                {
+                    _Id = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        string _Avatar;
+
+        public string Avatar
+        {
+            get { return _Avatar; }
+            set
+            {
+                if (_Avatar != value)
+                {
+                    _Avatar = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        string _Name;
+
+        public string Name
+        {
+            get { return _Name; }
+            set
+            {
+                if (_Name != value)
+                {
+                    _Name = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        string _Country;
+
+		public string Country
+		{
+			get { return _Country; }
+			set
+			{
+				if (_Country != value)
+				{
+					_Country = value;
+					RaisePropertyChanged();
+				}
+			}
+		}
+		
+        string _ImageSpeakLanguage;
+
+		public string ImageSpeakLanguage
+		{
+			get { return _ImageSpeakLanguage; }
+			set
+			{
+				if (_ImageSpeakLanguage != value)
+				{
+					_ImageSpeakLanguage = value;
+					RaisePropertyChanged();
+				}
+			}
+		}
+
+        string _ImageLearn;
+
+		public string ImageLearn
+		{
+			get { return _ImageLearn; }
+			set
+			{
+				if (_ImageLearn != value)
+				{
+					_ImageLearn = value;
+					RaisePropertyChanged();
+				}
+			}
+		}
+
+        string _TextLearn;
+
+		public string TextLearn
+		{
+			get { return _TextLearn; }
+			set
+			{
+				if (_TextLearn != value)
+				{
+					_TextLearn = value;
+					RaisePropertyChanged();
+				}
+			}
+		}
+
+        bool _Speak;
+
+        public bool Speak
+		{
+			get { return _Speak; }
+			set
+			{
+				if (_Speak != value)
+				{
+					_Speak = value;
+					RaisePropertyChanged();
+				}
+			}
+		}
+
+        string _Write;
+
+        public string Write
+		{
+			get { return _Write; }
+			set
+			{
+				if (_Write != value)
+				{
+					_Write = value;
+					RaisePropertyChanged();
+				}
+			}
+		}
+
+        string _PublicTime;
+
+		public string PublicTime
+		{
+			get { return _PublicTime; }
+			set
+			{
+				if (_PublicTime != value)
+				{
+					_PublicTime = value;
+					RaisePropertyChanged();
+				}
+			}
+		}
+
+        double _Star;
+
+        public double Star
+		{
+			get { return _Star; }
+			set
+			{
+				if (_Star != value)
+				{
+					_Star = value;
+					RaisePropertyChanged();
+				}
+			}
+		}
+
+        bool _Friends;
+
+		public bool Friends
+		{
+			get { return _Friends; }
+			set
+			{
+				if (_Friends != value)
+				{
+					_Friends = value;
+					RaisePropertyChanged();
+				}
+			}
+		}
+
+        string _ImgQuestion;
+
+		public string ImgQuestion
+		{
+			get { return _ImgQuestion; }
+			set
+			{
+				if (_ImgQuestion != value)
+				{
+					_ImgQuestion = value;
+					RaisePropertyChanged();
+				}
+			}
+		}
+
+        string _TextQuestion;
+
+		public string TextQuestion
+		{
+			get { return _TextQuestion; }
+			set
+			{
+				if (_TextQuestion != value)
+				{
+					_TextQuestion = value;
+					RaisePropertyChanged();
+				}
+			}
+		}
     }
 }
