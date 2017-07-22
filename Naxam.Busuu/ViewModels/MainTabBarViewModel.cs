65d55c43311901bb018e957a35b764b15359707b
Nguyen Nhu Son <son.nguyennhu@naxam.net>
2017-07-23 00:56:49 +0700

    add Naxam.Busuu

---
+++ Naxam.Busuu/ViewModels/MainTabBarViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MvvmCross.Core.ViewModels;
+using Naxam.Busuu.Notification.ViewModels;
+using Naxam.Busuu.Profile.ViewModel;
+using Naxam.Busuu.Review.ViewModels;
+using Naxam.Busuu.Social.ViewModels;
+
+
+namespace Naxam.Busuu.ViewModels
+{
+    public class MainTabBarViewModel : MvxViewModel
+    {
+		IMvxCommand _showInitialViewModelsCommand;
+		public IMvxCommand ShowInitialViewModelsCommand
+		{
+			get
+			{
+				return _showInitialViewModelsCommand ?? (_showInitialViewModelsCommand = new MvxCommand(ShowInitialViewModels));
+			}
+		}
+
+		void ShowInitialViewModels()
+		{
+            ShowViewModel<ReviewAllViewModel>();
+			ShowViewModel<SocialViewModel>();
+            ShowViewModel<NotificationViewModel>();
+            ShowViewModel<ProfileViewModel>();
+		}
+    }
+}
